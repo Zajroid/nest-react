e573af502eb7c392ebe3ab6e7922bbf2ea8de7db
Zajroid <zajroid@gmail.com>
2022-08-11 16:32:08 +0300

    add webpack

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,1 +1,8 @@
-const a = 1;
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return <h1> Hello world from react!</h1>;
+};
+
+render(<App />, document.getElementById('app'));
